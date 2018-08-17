f5282f5ef9880307ee0d0a916fa0651d16e9a8ff
zgjsjw <41534933+zgjsjw@users.noreply.github.com>
2018-08-17 22:37:48 +0800

    Update bread.py

--- bread.py
+++ bread.py
@@ -1,8 +1,26 @@
-data_in = 
-data_out =
+# -*- coding:utf-8 -*-
+import os
+import pandas as pd
+
+data_in = './data/FOOD_DATA.csv'
+data_out = './result'
+
+def clean_data(data_path):
+    raw_data = pd.read_csv(data_path)
+    #print(raw_data.head())
+    data_after_drop = raw_data.drop(['店铺所在城市'], axis=1)#丢弃上海，默认均为上海地域，保留店铺URL，留着查看玩玩
+    #print(len(data_after_drop))
+    data_after_dropna = data_after_drop.dropna()
+    #print(len(data_after_dropna))
+    data_after_dropdup = data_after_dropna.drop_duplicates()
+    #print(len(data_after_dropdup))
+    return data_after_dropdup
 
 def main():
-    print('test')
+    if not os.path.exists(data_out):
+        os.mkdir(data_out)
+
+    clean_data(data_in)
 
 
 if __name__ == '__main__':
